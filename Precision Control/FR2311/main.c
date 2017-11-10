c25588de6d5135ebc8aaebd7be1af41d5b0f7a65
jrepifano <jrepifano@gmail.com>
2017-11-10 12:36:22 -0500

    2311 code in

---
+++ Precision Control/FR2311/main.c
@@ -0,0 +1,118 @@
+#include <msp430.h>
+
+unsigned int ADC_Result;
+void Init_GPIO();
+
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;                                // Stop WDT
+
+    //Configure LEDs
+    Init_GPIO();
+    P1DIR |= BIT0;
+    P1OUT &= ~BIT0;
+    P2DIR |= BIT0;
+    P2OUT &= ~BIT0;
+
+    // Configure ADC A1 pin
+    P1SEL0 |= BIT1;
+    P1SEL1 |= BIT1;
+
+    // Disable the GPIO power-on default high-impedance mode to activate
+    // previously configured port settings
+    PM5CTL0 &= ~LOCKLPM5;
+
+    // Configure ADC10
+    ADCCTL0 |= ADCSHT_2 | ADCON;                             // ADCON, S&H=16 ADC clks
+    ADCCTL1 |= ADCSHP;                                       // ADCCLK = MODOSC; sampling timer
+    ADCCTL2 |= ADCRES;                                       // 10-bit conversion results
+    ADCMCTL0 |= ADCINCH_1;                                   // A1 ADC input select; Vref=AVCC
+    ADCIE |= ADCIE0;                                         // Enable ADC conv complete interrupt
+
+    //UART Set up
+    __bis_SR_register(SCG0);                 // disable FLL
+    CSCTL3 |= SELREF__REFOCLK;               // Set REFO as FLL reference source
+    CSCTL1 = DCOFTRIMEN_1 | DCOFTRIM0 | DCOFTRIM1 | DCORSEL_3;// DCOFTRIM=3, DCO Range = 8MHz
+    CSCTL2 = FLLD_0 + 243;                  // DCODIV = 8MHz
+    __delay_cycles(3);
+    __bic_SR_register(SCG0);                // enable FLL
+
+    CSCTL4 = SELMS__DCOCLKDIV | SELA__REFOCLK; // set default REFO(~32768Hz) as ACLK source, ACLK = 32768Hz
+                                             // default DCODIV as MCLK and SMCLK source
+
+    // Configure UART pins
+    P1SEL0 |= BIT6 | BIT7;                    // set 2-UART pin as second function
+
+    // Configure UART
+    UCA0CTLW0 |= UCSWRST;
+    UCA0CTLW0 |= UCSSEL__SMCLK;
+
+    // Baud Rate calculation
+    // 8000000/(16*9600) = 52.083
+    // Fractional portion = 0.083
+    // User's Guide Table 17-4: UCBRSx = 0x49
+    // UCBRFx = int ( (52.083-52)*16) = 1
+    UCA0BR0 = 52;                             // 8000000/16/9600
+    UCA0BR1 = 0x00;
+    UCA0MCTLW = 0x4900 | UCOS16 | UCBRF_1;
+
+    UCA0CTLW0 &= ~UCSWRST;                    // Initialize eUSCI
+
+    while(1)
+    {
+        ADCCTL0 |= ADCENC | ADCSC;                           // Sampling and conversion start
+        __bis_SR_register(LPM0_bits | GIE);                  // LPM0, ADC_ISR will force exit
+        __no_operation();                                    // For debug only
+        if (ADC_Result > 850) {
+        	P1OUT |= BIT0;
+        	P2OUT &= ~ BIT0;
+        }
+        else {
+        	P1OUT &= ~BIT0;
+        	P2OUT |= BIT0;
+        }                                   // Set P1.0 LED on
+        __delay_cycles(5000);
+    }
+}
+
+// ADC interrupt service routine
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=ADC_VECTOR
+__interrupt void ADC_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(ADC_VECTOR))) ADC_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+    switch(__even_in_range(ADCIV,ADCIV_ADCIFG))
+    {
+        case ADCIV_NONE:
+            break;
+        case ADCIV_ADCOVIFG:
+            break;
+        case ADCIV_ADCTOVIFG:
+            break;
+        case ADCIV_ADCHIIFG:
+            break;
+        case ADCIV_ADCLOIFG:
+            break;
+        case ADCIV_ADCINIFG:
+            break;
+        case ADCIV_ADCIFG:
+            ADC_Result = ADCMEM0;
+            UCA0TXBUF = (ADC_Result & 0xFF);
+            __bic_SR_register_on_exit(LPM0_bits);            // Clear CPUOFF bit from LPM0
+            break;
+        default:
+            break;
+    }
+}
+
+void Init_GPIO()
+{
+    P1DIR = 0xFF; P2DIR = 0xFF;
+    P1REN = 0xFF; P2REN = 0xFF;
+    P1OUT = 0x00; P2OUT = 0x00;
+}
+
